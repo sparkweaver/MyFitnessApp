4db39d898d304c9eaf81154e1f6eb27d23330072
RuneCodeIO <jl225ps@student.lnu.se>
2024-05-14 19:52:24 +0200

    Initial commit

---
+++ MyFitnessApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFitnessApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MyFitnessApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MyFitnessApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
