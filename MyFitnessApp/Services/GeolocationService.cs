7d7a30bbd70324dcdb2ca21897c60786af207112
RuneCodeIO <jl225ps@student.lnu.se>
2024-05-15 18:02:53 +0200

    feat: Created GeolocationSeervice that implementes methods from maui platform to get device location.

---
+++ MyFitnessApp/Services/GeolocationService.cs
@@ -0,0 +1,103 @@
+﻿using Android.Locations;
+using Microsoft.Maui.Devices.Sensors;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Location = Microsoft.Maui.Devices.Sensors.Location;
+
+namespace MyFitnessApp.Services;
+
+public class GeolocationService
+{
+    private CancellationTokenSource? _cancelTokenSource;
+    private bool _isCheckingLocation;
+
+    public async Task<string> GetCachedLocation()
+    {
+        try
+        {
+            Location location = await Geolocation.Default.GetLastKnownLocationAsync();
+
+            if (location != null)
+                return $"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}";
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
+        return "None";
+    }
+
+    public async Task GetCurrentLocation()
+    {
+        try
+        {
+            _isCheckingLocation = true;
+
+            GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+            _cancelTokenSource = new CancellationTokenSource();
+
+            Location location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+            if (location != null)
+                Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            _isCheckingLocation = false;
+        }
+    }
+
+    public void CancelRequest()
+    {
+        if (_isCheckingLocation && _cancelTokenSource != null && _cancelTokenSource.IsCancellationRequested == false)
+            _cancelTokenSource.Cancel();
+    }
+
+    async void OnStartListening()
+    {
+        try
+        {
+            Geolocation.LocationChanged += Geolocation_LocationChanged;
+            var request = new GeolocationListeningRequest(GeolocationAccuracy.Best);
+            var success = await Geolocation.StartListeningForegroundAsync(request);
+
+            string status = success
+                ? "Started listening for foreground location updates"
+                : "Couldn't start listening";
+            Console.WriteLine(status);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    void Geolocation_LocationChanged(object sender, GeolocationLocationChangedEventArgs e)
+    {
+        var location = e.Location;
+        Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+    }
+
+    void OnStopListening()
+    {
+        try
+        {
+            Geolocation.LocationChanged -= Geolocation_LocationChanged;
+            Geolocation.StopListeningForeground();
+            string status = "Stopped listening for foreground location updates";
+            Console.WriteLine(status);
+        }
+        catch (Exception ex)
+        {
+            // Unable to stop listening for location changes
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
